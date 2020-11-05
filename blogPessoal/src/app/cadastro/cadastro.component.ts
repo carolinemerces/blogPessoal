6e217d5c8b0ae1d66b842db2feb8c276e116af7a
unknown <carolinedasmerces@gmail.com>
2020-11-05 16:35:38 -0300

    Implementação das componentes cadastrar e login | navbar: botão sair | service: botão login, feed e sair

--- blogPessoal/src/app/cadastro/cadastro.component.ts
+++ blogPessoal/src/app/cadastro/cadastro.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
+  user: User = new User();
+  senha: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  conferirSenha(event:any){
+    this.senha=event.target.value
   }
 
+  cadastrar() {
+
+    if(this.senha === this.user.senha){
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp;
+      });
+      this.router.navigate(['/login']);
+      alert ('Cadastro efetuado com sucesso!')
+    }
+    else{
+      alert ('Suas senhas não conferem')
+    }
+    
+  }
 }
