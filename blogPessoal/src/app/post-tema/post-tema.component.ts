2448da68bbdd3eb57dd251f363d630d22eebabe8
unknown <carolinedasmerces@gmail.com>
2020-11-12 09:19:37 -0300

    Implementação dos Métodos Put e Delete (Postagens e Temas)

--- blogPessoal/src/app/post-tema/post-tema.component.ts
+++ blogPessoal/src/app/post-tema/post-tema.component.ts
@@ -44,4 +44,7 @@ export class PostTemaComponent implements OnInit {
     }
   }
 
+  voltar(){
+    this.router.navigate(['/feed'])
+  }
 }
---
+++ blogPessoal/src/app/put-postagem/put-postagem.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
+
+@Component({
+  selector: 'app-put-postagem',
+  templateUrl: './put-postagem.component.html',
+  styleUrls: ['./put-postagem.component.css']
+})
+export class PutPostagemComponent implements OnInit {
+
+  postagem: Postagem = new Postagem()
+  idPost: number
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  constructor(
+    private postagemService: PostagemService,
+     private temaService: TemaService, 
+     private router: Router,
+     private route: ActivatedRoute
+     ) { }
+
+  ngOnInit() {
+  window.scroll(0,0)
+
+    this.idPost = this.route.snapshot.params["id"]
+    this.findByIdPostagem(this.idPost)
+
+    this.findAllTemas();
+  }
+
+findByIdPostagem(id:number){
+  this.postagemService.getByIdPostagem(id).subscribe((resp : Postagem)=>{
+    this.postagem = resp
+  })
+}
+
+  salvar(){
+    this.tema.id = this.idTema
+    this.postagem.id = this.idTema
+
+    this.postagemService.putPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      this.router.navigate(['/feed'])
+      alert ('Postagem alterada com sucesso!')
+
+    }, err => {
+    if(err.status=='500'){
+      alert('Preencha todos os campos corretamente antes de enviar!')
+    }
+  })
+  }
+
+  voltar(){
+    this.router.navigate(['/feed'])
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp:Tema)=>{
+      this.tema = resp
+    })
+  }
+
+}
